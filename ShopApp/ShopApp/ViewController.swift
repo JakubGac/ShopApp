387714db11efb40d6cef90439f301f10e0697af8
Jakub Gac <gacus65@gmail.com>
2025-09-16 23:31:22 +0200

    Xcode initial commit

---
+++ ShopApp/ShopApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShopApp
+//
+//  Created by Jakub Gac on 16/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
